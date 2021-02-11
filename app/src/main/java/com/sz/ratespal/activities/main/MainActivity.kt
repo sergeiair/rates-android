fc9bcd85f37c6d61e8f793cb5d21564265c4d684
SergeiZZ <you@example.com>
2021-02-11 19:06:50 +0100

    go

---
+++ app/src/main/java/com/sz/ratespal/activities/main/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.sz.ratespal.activities.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import com.sz.ratespal.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+    }
+}
---
+++ app/src/main/java/com/sz/ratespal/ui/login/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.sz.ratespal.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sz.ratespal.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
